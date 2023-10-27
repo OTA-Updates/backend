35047b65ed0150cc20c140f4986862219e8c9c15
nikita zigman <zigman.nikita@gmail.com>
2023-10-27 17:19:51 +0100

    feat (colypte_api): adds dependencies to the tags's end points

--- backend/apps/calypte_api/calypte_api/tags/api/v1/routers.py
+++ backend/apps/calypte_api/calypte_api/tags/api/v1/routers.py
@@ -1,8 +1,11 @@
 from uuid import UUID
 
+from calypte_api.common.dependencies import JwtClaims, check_permission
+from calypte_api.common.user_roles import UserRole
 from calypte_api.tags import schemas as tags_schemas
+from calypte_api.tags.service import TagServiceType
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi_pagination import Page
 
 
@@ -11,7 +14,7 @@ router = APIRouter()
 
 @router.post(
     path="/tags",
-    response_model=tags_schemas.CreateTagRequestBody,
+    response_model=tags_schemas.CreateTagResponse,
     summary="Create a tag",
     description="create a tag",
     response_description="The created tag",
@@ -19,8 +22,13 @@ router = APIRouter()
 )
 async def create_tag(
     create_tag_request_body: tags_schemas.CreateTagRequestBody,
+    tag_service: TagServiceType,
+    jwt_claims: JwtClaims = Depends(check_permission(UserRole.USER)),
 ) -> tags_schemas.CreateTagResponse:
-    ...
+    return await tag_service.create_tag(
+        user_id=jwt_claims.user.id,
+        request_body=create_tag_request_body,
+    )
 
 
 @router.get(
@@ -31,8 +39,17 @@ async def create_tag(
     response_description="The page of tags",
     status_code=200,
 )
-async def get_tags_page() -> Page[tags_schemas.GetTagResponse]:
-    ...
+async def get_tags_page(
+    tag_service: TagServiceType,
+    query_params: tags_schemas.GetTagQueryParams = Depends(
+        tags_schemas.GetTagQueryParams
+    ),
+    jwt_claims: JwtClaims = Depends(check_permission(UserRole.USER)),
+) -> Page[tags_schemas.GetTagResponse]:
+    return await tag_service.get_tags(
+        user_id=jwt_claims.user.id,
+        query_params=query_params,
+    )
 
 
 @router.get(
@@ -43,22 +60,36 @@ async def get_tags_page() -> Page[tags_schemas.GetTagResponse]:
     response_description="The tag",
     status_code=200,
 )
-async def get_tag(tag_id: UUID) -> tags_schemas.GetTagResponse:
-    ...
+async def get_tag(
+    tag_id: UUID,
+    tag_service: TagServiceType,
+    jwt_claims: JwtClaims = Depends(check_permission(UserRole.USER)),
+) -> tags_schemas.GetTagResponse:
+    return await tag_service.get_tag(
+        user_id=jwt_claims.user.id,
+        tag_id=tag_id,
+    )
 
 
 @router.put(
     path="/tags/{tag_id:uuid}",
-    response_model=tags_schemas.UpdateTagRequestBody,
+    response_model=tags_schemas.UpdateTagResponse,
     summary="Update a tag",
     description="update a tag",
     response_description="The updated tag",
     status_code=200,
 )
 async def update_tag(
-    tag_id: UUID, update_tag_request_body: tags_schemas.UpdateTagRequestBody
+    tag_id: UUID,
+    update_tag_request_body: tags_schemas.UpdateTagRequestBody,
+    tag_service: TagServiceType,
+    jwt_claims: JwtClaims = Depends(check_permission(UserRole.USER)),
 ) -> tags_schemas.UpdateTagResponse:
-    ...
+    return await tag_service.update_tag(
+        user_id=jwt_claims.user.id,
+        tag_id=tag_id,
+        request_body=update_tag_request_body,
+    )
 
 
 @router.delete(
@@ -69,5 +100,12 @@ async def update_tag(
     response_description="The deleted tag",
     status_code=204,
 )
-async def delete_tag(tag_id: UUID) -> None:
-    ...
+async def delete_tag(
+    tag_id: UUID,
+    tag_service: TagServiceType,
+    jwt_claims: JwtClaims = Depends(check_permission(UserRole.USER)),
+) -> None:
+    await tag_service.delete_tag(
+        user_id=jwt_claims.user.id,
+        tag_id=tag_id,
+    )
