e1e1d51d8b1b2e41e1afe907e426798745aaaf50
nikita zigman <zigman.nikita@gmail.com>
2023-10-27 16:10:26 +0100

    feat (calypte_api): updates devices schemas

--- backend/apps/calypte_api/calypte_api/devices/schemas.py
+++ backend/apps/calypte_api/calypte_api/devices/schemas.py
@@ -1,48 +1,58 @@
 from datetime import datetime
 from uuid import UUID
 
-from pydantic import BaseModel, Field
+from fastapi_pagination import Params
+from pydantic import BaseModel, ConfigDict, Field
 
 
-class BaseDeviceSchema(BaseModel):
+class BaseDeviceRequestSchema(BaseModel):
     ...
 
 
-class CreateDeviceRequestBody(BaseDeviceSchema):
+class GetDeviceQueryParams(BaseDeviceRequestSchema, Params):
+    name: str | None = Field(alias="name", default=None)
+    # tags: list[UUID] | None = Field(alias="tags")
+
+
+class CreateDeviceRequestBody(BaseDeviceRequestSchema):
     name: str
     tags: list[UUID]
 
 
-class UpdateDeviceRequestBody(BaseDeviceSchema):
+class UpdateDeviceRequestBody(BaseDeviceRequestSchema):
     name: str
     tags: list[UUID]
 
 
-class CreateDeviceResponse(BaseDeviceSchema):
+class BaseDeviceResponseSchema(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
+
+
+class CreateDeviceResponse(BaseDeviceResponseSchema):
     id: UUID
     name: str
     tags: list[UUID]
 
-    registered_at: datetime = Field(alias="registeredAt")
+    registered_at: datetime | None = Field(alias="registeredAt")
     created_at: datetime = Field(alias="createdAt")
     updated_at: datetime = Field(alias="updatedAt")
 
 
-class UpdateDeviceResponse(BaseDeviceSchema):
+class UpdateDeviceResponse(BaseDeviceResponseSchema):
     id: UUID
     name: str
     tags: list[UUID]
 
-    registered_at: datetime = Field(alias="registeredAt")
+    registered_at: datetime | None = Field(alias="registeredAt")
     created_at: datetime = Field(alias="createdAt")
     updated_at: datetime = Field(alias="updatedAt")
 
 
-class GetDeviceResponse(BaseDeviceSchema):
+class GetDeviceResponse(BaseDeviceResponseSchema):
     id: UUID
     name: str
     tags: list[UUID]
 
-    registered_at: datetime = Field(alias="registeredAt")
+    registered_at: datetime | None = Field(alias="registeredAt")
     created_at: datetime = Field(alias="createdAt")
     updated_at: datetime = Field(alias="updatedAt")
