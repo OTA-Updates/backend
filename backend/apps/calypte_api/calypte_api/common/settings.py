e8c1995907c6217530e5858993ce49ae8a9f9aab
nikita zigman <zigman.nikita@gmail.com>
2023-10-27 16:05:26 +0100

    feat (calypte_api): adds settings for the jwt and rate limiter

--- backend/apps/calypte_api/calypte_api/common/settings.py
+++ backend/apps/calypte_api/calypte_api/common/settings.py
@@ -29,6 +29,21 @@ class Settings(BaseSettings):
     debug: bool = False
     logging_level: str = "INFO"
 
+    jwt_encoding_algorithm: str = "HS256"
+    jwt_secret_key: str
+
+    rate_limiter_times: int = 1000
+    rate_limiter_seconds: int = 60
+
+    def postgres_dsn(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}"
+            f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+        )
+
+    def redis_dsn(self) -> str:
+        return f"redis://{self.redis_host}:{self.redis_port}"
+
 
 @lru_cache(maxsize=1)
 def get_settings() -> Settings:
