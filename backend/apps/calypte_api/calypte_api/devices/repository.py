fd2c1f639c680a1e472f72ec7f11a7eebd76c17d
nikita zigman <zigman.nikita@gmail.com>
2023-11-01 21:44:41 +0000

    feat (calypte_api): adds repository for the devices

--- backend/apps/calypte_api/calypte_api/devices/repository.py
+++ backend/apps/calypte_api/calypte_api/devices/repository.py
@@ -1,84 +1,104 @@
-import random
-
 from abc import ABC, abstractmethod
-from datetime import datetime
-from typing import Annotated
-from uuid import UUID, uuid4
+from typing import Annotated, cast
+from uuid import UUID
 
 from calypte_api.common.dependencies import DBSessionType
+from calypte_api.common.models import device_tag_lookup
+from calypte_api.devices.models import Device
 from calypte_api.devices.schemas import (
     CreateDeviceResponse,
-    GetDeviceQueryParams,
     GetDeviceResponse,
     UpdateDeviceResponse,
 )
+from calypte_api.tags.models import Tag
 
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql import delete, insert, select, update
 
 
 class IDeviceRepo(ABC):
     @abstractmethod
     async def get_device_by_id(
-        self, user_id: UUID, device_id: UUID
-    ) -> GetDeviceResponse:
+        self, company_id: UUID, device_id: UUID
+    ) -> GetDeviceResponse | None:
         """
         Get device by id
 
         Args:
             device_id (UUID): device id
-            user_id (UUID): user id
+            company_id (UUID): user id
 
         """
 
     @abstractmethod
     async def get_devices(
-        self, user_id: UUID, query_params: GetDeviceQueryParams
+        self,
+        company_id: UUID,
+        type_id: UUID | None,
+        tags: list[UUID] | None,
+        firmware_info_id: UUID | None,
+        name: str | None,
+        offset: int,
+        limit: int,
     ) -> list[GetDeviceResponse]:
         """
         Get devices by query params
 
         Args:
-            user_id (UUID): user id
-            query_params (GetDeviceQueryParams): query params
+            company_id (UUID): user id
+            tags (list[UUID]): device tags
+            name (str | None): device name
+            offset (int): offset
+            limit (int): limit
         """
 
     @abstractmethod
     async def create_device(
         self,
-        user_id: UUID,
-        tags: list[UUID],
+        type_id: UUID,
+        company_id: UUID,
+        name: str,
+        description: str | None,
+        firmware_info_id: UUID | None,
+        tags: list[UUID] | None,
     ) -> CreateDeviceResponse:
         """
         Create device
 
         Args:
-            user_id (UUID): user id
+            company_id (UUID): user id
             name (str): device name
             tags (list[UUID]): device tags
         """
 
     @abstractmethod
     async def update_device(
-        self, user_id: UUID, device_id: UUID, tags: list[UUID]
+        self,
+        device_id: UUID,
+        company_id: UUID,
+        firmware_info_id: UUID | None,
+        name: str | None,
+        description: str | None,
+        tags: list[UUID] | None,
     ) -> UpdateDeviceResponse:
         """
         Update device
 
         Args:
-            user_id (UUID): user id
+            company_id (UUID): user id
             device_id (UUID): device id
             name (str): device name
             tags (list[UUID]): device tags
         """
 
     @abstractmethod
-    async def delete_device(self, user_id: UUID, device_id: UUID) -> None:
+    async def delete_device(self, company_id: UUID, device_id: UUID) -> None:
         """
         Delete device
 
         Args:
-            user_id (UUID): user id
+            company_id (UUID): user id
             device_id (UUID): device id
         """
 
@@ -87,73 +107,191 @@ class DeviceRepo(IDeviceRepo):
     def __init__(self, db_session: AsyncSession) -> None:
         self.db_session = db_session
 
-    # TODO: implement
     async def get_device_by_id(
-        self, user_id: UUID, device_id: UUID
-    ) -> GetDeviceResponse:
+        self, company_id: UUID, device_id: UUID
+    ) -> GetDeviceResponse | None:
+        select_stmt = (
+            select(Device)
+            .join(Device.tags, isouter=True)
+            .where(Device.company_id == company_id)
+            .where(Device.id == device_id)
+        )
+        result = await self.db_session.scalar(select_stmt)
+
+        if result is None:
+            return None
+
+        tags = [tag.id for tag in result.tags]
         return GetDeviceResponse(
-            id=device_id,
-            registered_at=None,
-            type_id=uuid4(),
-            tags=[uuid4(), uuid4(), uuid4()],
-            firmware_info=[uuid4(), uuid4()],
-            created_at=datetime.now(),
-            updated_at=datetime.now(),
+            id=result.id,
+            name=result.name,
+            description=result.description,
+            registered_at=result.registered_at,
+            company_id=result.company_id,
+            type_id=result.type_id,
+            tags=tags,
+            firmware_info_id=result.firmware_info_id,
+            created_at=result.created_at,
+            updated_at=result.updated_at,
         )
 
-    # TODO: implement
     async def get_devices(
-        self, user_id: UUID, query_params: GetDeviceQueryParams
+        self,
+        company_id: UUID,
+        type_id: UUID | None,
+        tags: list[UUID] | None,
+        firmware_info_id: UUID | None,
+        name: str | None,
+        offset: int,
+        limit: int,
     ) -> list[GetDeviceResponse]:
-        tags = [uuid4() for _ in range(10)]
-        firmware_info = [uuid4() for _ in range(10)]
-        return [
-            GetDeviceResponse(
-                id=uuid4(),
-                registered_at=None if random.randint(0, 1) == 0 else datetime.now(),
-                type_id=uuid4(),
-                tags=[random.choice(tags) for _ in range(random.randint(1, 3))],
-                firmware_info=[random.choice(firmware_info) for _ in range(3)],
-                created_at=datetime.now(),
-                updated_at=datetime.now(),
+        select_stmt = (
+            select(Device)
+            .join(Device.tags, isouter=True)
+            .where(Device.company_id == company_id)
+            .order_by(Device.created_at)
+        )
+
+        if firmware_info_id is not None:
+            select_stmt = select_stmt.where(
+                Device.firmware_info_id == firmware_info_id
             )
-            for _ in range(query_params.size)
+
+        if tags is not None:
+            select_stmt = select_stmt.where(Tag.id.in_(tags))
+
+        if type_id is not None:
+            select_stmt = select_stmt.where(Device.type_id == type_id)
+
+        if name is not None:
+            select_stmt = select_stmt.where(Device.name == name)
+
+        select_stmt = (
+            select_stmt.group_by(Device.id).offset(offset).limit(limit)
+        )
+
+        results = await self.db_session.scalars(select_stmt)
+
+        get_devices_schemas = [
+            GetDeviceResponse(
+                id=result.id,
+                name=result.name,
+                description=result.description,
+                registered_at=result.registered_at,
+                company_id=result.company_id,
+                type_id=result.type_id,
+                tags=[tag.id for tag in result.tags],
+                firmware_info_id=result.firmware_info_id,
+                created_at=result.created_at,
+                updated_at=result.updated_at,
+            )
+            for result in results.unique()
         ]
 
-    # TODO: implement
+        return get_devices_schemas
+
     async def create_device(
         self,
-        user_id: UUID,
-        tags: list[UUID],
+        type_id: UUID,
+        company_id: UUID,
+        name: str,
+        description: str | None,
+        firmware_info_id: UUID | None,
+        tags: list[UUID] | None,
     ) -> CreateDeviceResponse:
+        insert_device_stmt = (
+            insert(Device)
+            .values(
+                type_id=type_id,
+                firmware_info_id=firmware_info_id,
+                name=name,
+                description=description,
+                company_id=company_id,
+            )
+            .returning(Device)
+        )
+        device_result = await self.db_session.execute(insert_device_stmt)
+        new_device = cast(Device, device_result.scalar())
+
+        if tags is not None:
+            tag_device_lookup_values = [
+                {"device_id": new_device.id, "tag_id": tag_id}
+                for tag_id in tags
+            ]
+            insert_device_tags_stmt = insert(device_tag_lookup).values(
+                tag_device_lookup_values
+            )
+            await self.db_session.execute(insert_device_tags_stmt)
+
         return CreateDeviceResponse(
-            id=uuid4(),
-            registered_at=None,
-            type_id=uuid4(),
-            tags=tags,
-            firmware_info=[uuid4(), uuid4()],
-            created_at=datetime.now(),
-            updated_at=datetime.now(),
+            id=new_device.id,
+            name=new_device.name,
+            description=new_device.description,
+            registered_at=new_device.registered_at,
+            company_id=new_device.company_id,
+            type_id=new_device.type_id,
+            tags=[tag.id for tag in await new_device.awaitable_attrs.tags],
+            firmware_info_id=new_device.firmware_info_id,
+            created_at=new_device.created_at,
+            updated_at=new_device.updated_at,
         )
 
-    # TODO: implement
     async def update_device(
-        self, user_id: UUID, device_id: UUID, tags: list[UUID]
+        self,
+        device_id: UUID,
+        company_id: UUID,
+        firmware_info_id: UUID | None,
+        name: str | None,
+        description: str | None,
+        tags: list[UUID] | None,
     ) -> UpdateDeviceResponse:
-        registered_at = None if random.randint(0, 1) == 0 else datetime.now()
+        if tags is not None:
+            delete_device_tags_stmt = delete(device_tag_lookup).where(
+                device_tag_lookup.c.device_id == device_id
+            )
+            await self.db_session.execute(delete_device_tags_stmt)
+
+            insert_device_tags_stmt = device_tag_lookup.insert().values(
+                [{"device_id": device_id, "tag_id": tag_id} for tag_id in tags]
+            )
+            await self.db_session.execute(insert_device_tags_stmt)
+
+        insert_device_stmt = (
+            update(Device)
+            .where(Device.company_id == company_id)
+            .where(Device.id == device_id)
+            .values(
+                id=device_id,
+                name=name,
+                description=description,
+                company_id=company_id,
+                firmware_info_id=firmware_info_id,
+            )
+            .returning(Device)
+        )
+        device_result = await self.db_session.execute(insert_device_stmt)
+        new_device = device_result.scalar()
+
         return UpdateDeviceResponse(
-            id=device_id,
-            registered_at=registered_at,
-            type_id=uuid4(),
-            tags=tags,
-            firmware_info=[uuid4(), uuid4()],
-            created_at=datetime.now(),
-            updated_at=datetime.now(),
+            id=new_device.id,
+            name=new_device.name,
+            description=new_device.description,
+            registered_at=new_device.registered_at,
+            company_id=new_device.company_id,
+            type_id=new_device.type_id,
+            tags=[tag.id for tag in await new_device.awaitable_attrs.tags],
+            firmware_info_id=new_device.firmware_info_id,
+            created_at=new_device.created_at,
+            updated_at=new_device.updated_at,
         )
 
-    # TODO: implement
-    async def delete_device(self, user_id: UUID, device_id: UUID) -> None:
-        return None
+    async def delete_device(self, company_id: UUID, device_id: UUID) -> None:
+        delete_stmt = (
+            delete(Device)
+            .where(Device.company_id == company_id)
+            .where(Device.id == device_id)
+        )
+        await self.db_session.execute(delete_stmt)
 
 
 def get_device_repo(db_session: DBSessionType) -> IDeviceRepo:
---
+++ backend/apps/calypte_api/tests/functional/src/devices/test_repositories.py
@@ -0,0 +1,283 @@
+import random
+
+from datetime import datetime
+from uuid import uuid4
+
+import pytest
+import pytest_asyncio
+
+from calypte_api.common.models import device_tag_lookup
+from calypte_api.devices.models import Device
+from calypte_api.devices.repository import DeviceRepo
+from calypte_api.firmware_info.models import FirmwareInfo
+from calypte_api.tags.models import Tag
+from calypte_api.types.models import Type
+from sqlalchemy import insert
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+@pytest_asyncio.fixture(scope="function")
+async def device_repo(db_session: AsyncSession):
+    return DeviceRepo(db_session=db_session)
+
+
+@pytest_asyncio.fixture(scope="function")
+async def test_device_data(
+    db_session: AsyncSession,
+):
+    company_ids = [uuid4() for _ in range(2)]
+    type_values = [
+        {
+            "id": uuid4(),
+            "company_id": random.choice(company_ids),
+            "name": f"test name {i}",
+            "description": f"test description {i}",
+            "created_at": datetime.now(),
+        }
+        for i in range(10)
+    ]
+    await db_session.execute(insert(Type).values(type_values))
+
+    tag_values = []
+    for i in range(100):
+        type_obj = random.choice(type_values)
+        tag_values.append(
+            {
+                "id": uuid4(),
+                "company_id": type_obj["company_id"],
+                "type_id": type_obj["id"],
+                "name": f"test name {i}",
+                "color": "#000000",
+                "created_at": datetime.now(),
+                "updated_at": datetime.now(),
+            }
+        )
+    await db_session.execute(insert(Tag).values(tag_values))
+
+    firmware_values = []
+    for i in range(100):
+        type_obj = random.choice(type_values)
+        firmware_values.append(
+            {
+                "id": uuid4(),
+                "company_id": type_obj["company_id"],
+                "type_id": type_obj["id"],
+                "name": f"test name {i}",
+                "description": f"test firmware description {i}",
+                "version": "v1.0.0",
+                "created_at": datetime.now(),
+                "updated_at": datetime.now(),
+            }
+        )
+    await db_session.execute(insert(FirmwareInfo).values(firmware_values))
+
+    device_values = []
+    for i in range(100):
+        type_obj = random.choice(type_values)
+        firmware_obj = random.choice(firmware_values)
+        device_values.append(
+            {
+                "id": uuid4(),
+                "company_id": type_obj["company_id"],
+                "type_id": type_obj["id"],
+                "firmware_info_id": firmware_obj["id"],
+                "registered_at": random.choice([datetime.now(), None]),
+                "name": f"test name {i}",
+                "description": f"test description {i}",
+                "created_at": datetime.now(),
+                "updated_at": datetime.now(),
+            }
+        )
+    await db_session.execute(insert(Device).values(device_values))
+
+    device_tag_values = []
+    for device in device_values:
+        tags_ids = {tag["id"] for tag in random.choices(tag_values, k=4)}
+        device["tags"] = list(tags_ids)
+        for tag_id in tags_ids:
+            device_tag_values.append(
+                {
+                    "device_id": device["id"],
+                    "tag_id": tag_id,
+                }
+            )
+    await db_session.execute(
+        insert(device_tag_lookup).values(device_tag_values)
+    )
+
+    await db_session.commit()
+    return device_values
+
+
+async def test_get_device_by_id(
+    test_device_data: list[dict],
+    device_repo: DeviceRepo,
+) -> None:
+    expected_device = test_device_data[0]
+    device_object = await device_repo.get_device_by_id(
+        company_id=expected_device["company_id"],
+        device_id=expected_device["id"],
+    )
+    assert device_object.id == expected_device["id"]
+    assert device_object.name == expected_device["name"]
+    assert device_object.description == expected_device["description"]
+    assert device_object.registered_at == expected_device["registered_at"]
+    assert device_object.company_id == expected_device["company_id"]
+    assert device_object.type_id == expected_device["type_id"]
+    assert (
+        device_object.firmware_info_id == expected_device["firmware_info_id"]
+    )
+
+
+@pytest.mark.parametrize(
+    "limit, offset, name, type_id_filter, tags_filter, firmware_info_filter",
+    [
+        (5, 1, None, False, False, False),
+        (5, 0, "test name 1", False, False, False),
+        (5, 0, None, True, False, False),
+        (5, 0, None, False, True, False),
+        (5, 0, None, False, False, True),
+        (5, 0, "test name 1", True, True, True),
+    ],
+)
+async def test_get_devices(
+    test_device_data: list[dict],
+    device_repo: DeviceRepo,
+    limit: int,
+    offset: int,
+    name: str | None,
+    type_id_filter: bool,
+    tags_filter: bool,
+    firmware_info_filter: bool,
+) -> None:
+    command_id = test_device_data[0]["company_id"]
+    type_id = None
+    tags = None
+    firmware_info_id = None
+
+    if type_id_filter:
+        type_id = test_device_data[0]["type_id"]
+    if tags_filter:
+        tags = test_device_data[0]["tags"][:1]
+    if firmware_info_filter:
+        firmware_info_id = test_device_data[0]["firmware_info_id"]
+
+    device_objects = await device_repo.get_devices(
+        company_id=command_id,
+        type_id=type_id,
+        firmware_info_id=firmware_info_id,
+        name=name,
+        tags=tags,
+        limit=limit,
+        offset=offset,
+    )
+
+    expected_devices = [
+        device_data
+        for device_data in test_device_data
+        if (device_data["company_id"] == command_id)
+        and (name is None or device_data["name"] == name)
+        and (type_id is None or device_data["type_id"] == type_id)
+        and (tags is None or set(tags).issubset(device_data["tags"]))
+        and (
+            firmware_info_id is None
+            or device_data["firmware_info_id"] == firmware_info_id
+        )
+    ]
+
+    expected_devices = expected_devices[offset : offset + limit]
+    expected_devices.sort(key=lambda x: x["created_at"])
+
+    assert len(device_objects) == len(expected_devices)
+
+    for device_object, expected_device in zip(
+        device_objects, expected_devices
+    ):
+        assert device_object.id == expected_device["id"]
+        assert device_object.name == expected_device["name"]
+        assert device_object.description == expected_device["description"]
+
+
+@pytest.mark.parametrize(
+    "device",
+    [
+        {
+            "name": "test name",
+            "description": "test description",
+            "company_id": uuid4(),
+        }
+    ],
+)
+async def test_create_device(
+    device_repo: DeviceRepo,
+    test_device_data: list[dict],
+    device: dict,
+) -> None:
+    expected_device = test_device_data[0]
+
+    new_device = {
+        **device,
+        "type_id": expected_device["type_id"],
+        "tags": expected_device["tags"],
+        "firmware_info_id": expected_device["firmware_info_id"],
+    }
+
+    device_object = await device_repo.create_device(**new_device)
+    assert device_object.name == device["name"]
+    assert device_object.description == device["description"]
+    assert device_object.company_id == device["company_id"]
+
+
+@pytest.mark.parametrize(
+    "device",
+    [
+        {
+            "name": "test name",
+            "description": "test description",
+        }
+    ],
+)
+async def test_update_device(
+    test_device_data: list[dict],
+    device_repo: DeviceRepo,
+    device: dict,
+) -> None:
+    expected_device = test_device_data[0]
+
+    new_device = {
+        **device,
+        "company_id": expected_device["company_id"],  # type: ignore
+        "device_id": expected_device["id"],
+        "tags": expected_device["tags"],
+        "firmware_info_id": expected_device["firmware_info_id"],
+    }
+
+    device_object = await device_repo.update_device(**new_device)
+
+    assert device_object.name == new_device["name"]
+    assert device_object.description == new_device["description"]
+    assert device_object.id == new_device["device_id"]
+
+
+async def test_delete_device(
+    test_device_data: list[dict],
+    device_repo: DeviceRepo,
+) -> None:
+    device_id = test_device_data[0]["id"]
+    company_id = test_device_data[0]["company_id"]
+
+    device_object = await device_repo.get_device_by_id(
+        device_id=device_id,
+        company_id=company_id,
+    )
+    assert device_object is not None
+
+    await device_repo.delete_device(
+        device_id=device_id,
+        company_id=company_id,
+    )
+    device_object = await device_repo.get_device_by_id(
+        device_id=device_id,
+        company_id=company_id,
+    )
+    assert device_object is None
