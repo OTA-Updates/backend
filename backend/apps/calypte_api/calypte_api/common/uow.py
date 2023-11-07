2e700da7f5713ed08d4372fdc1d914308112bcba
nikita zigman <zigman.nikita@gmail.com>
2023-11-07 23:59:45 +0000

    feat (calypte_api): adds common uow implementation

--- backend/apps/calypte_api/calypte_api/common/uow.py
+++ backend/apps/calypte_api/calypte_api/common/uow.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 from types import TracebackType
 from typing import Self
 
+from sqlalchemy.ext.asyncio import AsyncSession
+
 
 class IUOW(ABC):
     @abstractmethod
@@ -24,3 +26,27 @@ class IUOW(ABC):
     @abstractmethod
     async def rollback(self):
         ...
+
+
+class UOW(IUOW):
+    def __init__(self, sql_session: AsyncSession) -> None:
+        self.sql_session = sql_session
+
+    async def __aenter__(self) -> Self:
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: type[Exception],
+        exc_val: Exception,
+        exc_tb: TracebackType,
+    ):
+        if exc_val is not None:
+            await self.rollback()
+            raise exc_val
+
+    async def commit(self):
+        await self.sql_session.commit()
+
+    async def rollback(self):
+        await self.sql_session.rollback()
