e7fd964d9ffd72d8fe06877ffc99bb90140e6532
nikita zigman <zigman.nikita@gmail.com>
2024-02-20 20:56:49 +0000

    feat calypte_api: add group field to a tag model

--- backend/apps/calypte_api/calypte_api/tag/models.py
+++ backend/apps/calypte_api/calypte_api/tag/models.py
@@ -1,8 +1,12 @@
 from calypte_api.models import TimeStampAbstract, UUIDAbstract
 from colorfield.fields import ColorField  # type: ignore
+from device_group.models import DeviceGroup
 from django.db import models
 
 
 class Tag(UUIDAbstract, TimeStampAbstract):  # type: ignore
     name: models.CharField = models.CharField(max_length=255, null=False, blank=False)
     color: ColorField = ColorField(default="#FF0000")
+    group: models.ForeignKey = models.ForeignKey(
+        DeviceGroup, on_delete=models.CASCADE, related_name="tags"
+    )
