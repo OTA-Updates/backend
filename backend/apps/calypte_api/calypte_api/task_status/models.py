061f66d0175350cf6923046c2a8c9074f74ea232
nikita zigman <zigman.nikita@gmail.com>
2024-02-20 20:55:58 +0000

    feat calypte_api: migrate state model from char to int field

--- backend/apps/calypte_api/calypte_api/task_status/models.py
+++ backend/apps/calypte_api/calypte_api/task_status/models.py
@@ -2,18 +2,20 @@ from calypte_api.models import TimeStampAbstract, UUIDAbstract
 from django.db import models
 
 
-class TaskStatusChoices(models.TextChoices):
-    PLANNED = "PLANNED", "PLANNED"
-    PENDING = "PENDING", "PENDING"
-    RUNNING = "RUNNING", "RUNNING"
-    COMPLETED = "COMPLETED", "COMPLETED"
-    FAILED = "FAILED", "FAILED"
+class TaskStatusChoices(models.IntegerChoices):
+    PLANNED = 2
+    PENDING = 3
+    RUNNING = 4
+    COMPLETED = 5
+    FAILED = 1
 
 
 class TaskStatus(UUIDAbstract, TimeStampAbstract):  # type: ignore
-    state: models.CharField = models.CharField(
-        max_length=255,
+    state: models.IntegerField = models.IntegerField(
         null=False,
         blank=False,
         choices=TaskStatusChoices,
     )
+
+    def __str__(self) -> str:
+        return TaskStatusChoices(self.state).label
